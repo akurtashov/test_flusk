5c250d31488d4e363790a4831cf9085bea2c6b6d
arthur <6036916@mail.ru>
2021-01-06 21:54:28 +0300

    initial commit

---
+++ t1.py
@@ -0,0 +1,14 @@
+import sqlite3
+import datetime
+
+
+def first():
+    pass
+
+def fname(arg):
+    day = datetime.date.today()
+    print(day)
+
+if __name__ == '__main__':
+    first()
+    print(day)
